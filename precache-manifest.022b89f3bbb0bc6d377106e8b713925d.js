4c6f7c5ea98290f0b99ae46ba7e65d08f3e08215
shivam1192 <sg9827252555@gmail.com>
2020-05-23 00:48:47 +0530

    Updates

---
+++ precache-manifest.022b89f3bbb0bc6d377106e8b713925d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddbac2827d748ff3a5d104c3d5472bb",
+    "url": "/shivam1192/React-UI.git/index.html"
+  },
+  {
+    "revision": "66284707da123758ae82",
+    "url": "/shivam1192/React-UI.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dfde0f73480877080f48",
+    "url": "/shivam1192/React-UI.git/static/js/2.31165488.chunk.js"
+  },
+  {
+    "revision": "ba5d74fe07f11271076d1226c193885e",
+    "url": "/shivam1192/React-UI.git/static/js/2.31165488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66284707da123758ae82",
+    "url": "/shivam1192/React-UI.git/static/js/main.a77ff734.chunk.js"
+  },
+  {
+    "revision": "a187f6163ed6b3b2f7dd",
+    "url": "/shivam1192/React-UI.git/static/js/runtime-main.7129d58b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/shivam1192/React-UI.git/precache-manifest.022b89f3bbb0bc6d377106e8b713925d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shivam1192/React-UI.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
